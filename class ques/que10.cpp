bc73e321ffcab05c20c58929846a5ff4fdf40610
darshan sagare <87358897+darshan-111@users.noreply.github.com>
2023-08-13 21:02:34 +0530

    Add files via upload

---
+++ class ques/que10.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int per(int a, int b){
+    int c;
+    if (a > b)
+    {
+        c = 100/a*b;
+        return c;
+    }
+    else{
+        c = 100/b*a;
+        return c;
+    }
+    
+
+}
+int main(){
+    int num1, num2;
+    cout<<"please enter num 1 ";
+    cin>>num1;
+    cout<<"please enter num 2 ";
+    cin>>num2;
+    int result = per(num1, num2);
+    cout<<result;
+    return 0;
+}
---
+++ class ques/que11.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int swap(int a, int b){
+    return b; a;
+}
+int main(){
+    int e = swap(8, 7);
+    cout<<e;
+
+}
